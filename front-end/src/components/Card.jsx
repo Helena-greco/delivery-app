ea3a8e05099f327399a46f120e6aaf0d0ce2cbb1
Julio <julioaranabr@yahoo.com.br>
2022-05-04 23:24:50 -0300

    implemented quantity products feat

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,51 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button, Col, InputGroup, FormControl } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const CardComponent = ({ id, name, price, urlImage }) => (
-  <Col className="text-center">
-    <Card style={ { width: '18rem', margin: 'auto' } }>
-      <Card.Img
-        data-testid={ `customer_products__img-card-bg-image-${id}` }
-        src={ urlImage }
-      />
-      <Card.Body>
-        <Card.Title
-          data-testid={ `customer_products__element-card-title-${id}` }
-        >
-          { name }
-        </Card.Title>
-        <Card.Text
-          data-testid={ `customer_products__element-card-price-${id}` }
-        >
-          { price.replace('.', ',') }
-        </Card.Text>
-        <InputGroup
-          className="mb-3 w-50"
-          style={ { margin: 'auto' } }
-        >
-          <Button
-            variant="primary"
-            data-testid={ `customer_products__button-card-rm-item-${id}` }
+const CardComponent = ({ id, name, price, urlImage }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleDecrease = () => {
+    setQuantity(quantity - 1);
+  };
+
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  return (
+    <Col className="text-center">
+      <Card style={ { width: '18rem', margin: 'auto' } }>
+        <Card.Img
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
+          src={ urlImage }
+        />
+        <Card.Body>
+          <Card.Title
+            data-testid={ `customer_products__element-card-title-${id}` }
           >
-            -
-          </Button>
-          <FormControl
-            type="number"
-            className="text-center"
-            data-testid={ `customer_products__input-card-quantity-${id}` }
-          />
-          <Button
-            variant="primary"
-            data-testid={ `customer_products__button-card-add-item-${id}` }
+            { name }
+          </Card.Title>
+          <Card.Text
+            data-testid={ `customer_products__element-card-price-${id}` }
           >
-            +
-          </Button>
-        </InputGroup>
-      </Card.Body>
-    </Card>
-  </Col>
-);
+            { price.replace('.', ',') }
+          </Card.Text>
+          <InputGroup
+            className="mb-3 w-50"
+            style={ { margin: 'auto' } }
+          >
+            <Button
+              variant="primary"
+              data-testid={ `customer_products__button-card-rm-item-${id}` }
+              onClick={ handleDecrease }
+            >
+              -
+            </Button>
+            <FormControl
+              type="number"
+              className="text-center"
+              data-testid={ `customer_products__input-card-quantity-${id}` }
+              value={ quantity }
+            />
+            <Button
+              variant="primary"
+              data-testid={ `customer_products__button-card-add-item-${id}` }
+              onClick={ handleIncrease }
+            >
+              +
+            </Button>
+          </InputGroup>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
 CardComponent.propTypes = {
   id: PropTypes.number.isRequired,
