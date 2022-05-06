fab412b87e1fb90b515028103dffd1e759cde888
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-05-06 16:56:10 -0400

    feat: remove item ao clicar no botão

--- front-end/src/components/TableProducts.jsx
+++ front-end/src/components/TableProducts.jsx
@@ -1,28 +1,24 @@
 import React, { useEffect } from 'react';
 import { Button, Table } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 const TableProducts = ({ itemsCard, setItemsCard }) => {
-
   const dataStorage = JSON.parse(localStorage.getItem('carShop')) || [];
-  const filteredItems = dataStorage.filter((product) =>product.totalCard > 0);
+  const filteredItems = dataStorage.filter((product) => product.totalCard > 0);
 
   useEffect(() => {
-    
-  }, [itemsCard]);
+    setItemsCard(filteredItems);
+  }, []);
+  console.log(itemsCard);
 
   const handleItems = (index) => {
     const items = filteredItems;
     items.splice(index, 1);
     localStorage.setItem('carShop', JSON.stringify(items));
     setItemsCard(items);
-  }
-
-  // useEffect(() => {
-  //   handleItems()
-  // }, [itemsCard]);
+  };
 
   return (
-  <>
     <Table striped bordered hover>
       <thead>
         <tr>
@@ -38,14 +34,38 @@ const TableProducts = ({ itemsCard, setItemsCard }) => {
         { itemsCard.length && itemsCard.map((product, index) => (
           <tr key={ index }>
             <td>{index + 1}</td>
-            <td data-testid={`customer_checkout__element-order-table-name-${index + 1}`}>{product.name}</td>
-            <td data-testid={`customer_checkout__element-order-table-quantity-${index + 1}`}>{product.quantity}</td>
-            <td data-testid={`customer_checkout__element-order-table-unit-price-${index + 1}`}>{product.price}</td>
-            <td data-testid={`customer_checkout__element-order-table-sub-total-${index + 1}`}>{product.totalCard}</td>
+            <td
+              data-testid={ `customer_checkout__element-order-table-name-${index + 1}` }
+            >
+              {product.name}
+            </td>
+            <td
+              data-testid={
+                `customer_checkout__element-order-table-quantity-${index + 1}`
+              }
+            >
+              {product.quantity}
+            </td>
+            <td
+              data-testid={
+                `customer_checkout__element-order-table-unit-price-${index + 1}`
+              }
+            >
+              {product.price}
+            </td>
+            <td
+              data-testid={
+                `customer_checkout__element-order-table-sub-total-${index + 1}`
+              }
+            >
+              {product.totalCard}
+            </td>
             <td>
               <Button
-                data-testid={`customer_checkout__element-order-table-remove-${index + 1}`}
-                onClick={() => { handleItems(index)}}
+                data-testid={
+                  `customer_checkout__element-order-table-remove-${index + 1}`
+                }
+                onClick={ () => { handleItems(index) } }
               >
                 Remover
               </Button>
@@ -54,8 +74,12 @@ const TableProducts = ({ itemsCard, setItemsCard }) => {
         )) }
       </tbody>
     </Table>
-  </>
   );
-}
+};
+
+/* TableProducts.propTypes = {
+  itemsCard: PropTypes. ,
+  setItemsCard: PropTypes.func,
+}; */
 
 export default TableProducts;
