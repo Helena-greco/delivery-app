a0311243d4e249ae1061e746db26eb37673692f6
Israel Nazareth <israel.fab@hotmail.com>
2022-05-05 23:28:32 -0300

    fix: really, lint?

--- front-end/src/pages/CheckoutCustomer.jsx
+++ front-end/src/pages/CheckoutCustomer.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const CheckoutCustomer = () => {
-  return (
+const CheckoutCustomer = () => (
     <div>
       <h1>Finalização da compra do cliente</h1>
     </div>
-  )
-};
+  );
 
 export default CheckoutCustomer;
