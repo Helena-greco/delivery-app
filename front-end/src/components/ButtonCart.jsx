9d3f55b0db3b17764bdfa29bd95ce82172145cf5
Vinicius <vinicius.pereira@scaleup.com.br>
2022-05-06 11:51:15 -0300

    Recebe totalCart como props e formata moeda para real

--- front-end/src/components/ButtonCart.jsx
+++ front-end/src/components/ButtonCart.jsx
@@ -2,20 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-const ButtonCart = () => {
+const ButtonCart = ({ totalCart }) => {
   const navigate = useNavigate();
-  const [totalPrice, setTotalPrice] = useState(0);
 
-  const getTotalPrice = () => {
-    const dataStorage = JSON.parse(localStorage.getItem('carShop')) || [];
-    const total = dataStorage.reduce((acc, product) => acc + product.totalCard, 0);
-    const formatedNumber = Number(total).toFixed(2).replace('.', ',');
-    setTotalPrice(formatedNumber);
-  };
-
-  useEffect(() => {
-    getTotalPrice();
-  }, [totalPrice]);
 
   return (
     <Button
@@ -26,11 +15,10 @@ const ButtonCart = () => {
       onClick={ () => { navigate('/customer/checkout'); } }
       className="position-fixed bottom-0 end-0 m-3 z-index-auto"
       style={ { zIndex: 3 } }
-      disabled={ parseInt(totalPrice, 10) === 0 }
+      disabled={ parseInt(totalCart, 10) === 0 }
     >
       <span data-testid="customer_products__checkout-bottom-value">
-        Ver Carrinho: R$
-        { totalPrice }
+        {`Ver Carrinho: ${ totalCart }`}
       </span>
     </Button>
   );
