a5b38a759de58407a64b12351bb06f33e0b774e0
Vinicius <vinicius.pereira@scaleup.com.br>
2022-05-10 11:29:28 -0300

    feat: rota para direcionar para detalhes do pedido

--- front-end/src/components/TableSales.jsx
+++ front-end/src/components/TableSales.jsx
@@ -1,60 +1,73 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
 import '../style/TableProducts.css';
 import PropTypes from 'prop-types';
 
-const TableProducts = ({ sales }) => (
-  <Table striped bordered hover>
-    <thead>
-      <tr className="text-center">
-        <th>Pedido</th>
-        <th>Status</th>
-        <th>Data</th>
-        <th>Valor Total</th>
-      </tr>
-    </thead>
-    <tbody>
-      { sales.length ? sales.map(({ id, status, saleDate, totalPrice }, index) => (
-        <tr key={ index }>
-          <td
-            className="text-center"
-            width={ 1 }
-            data-testid={
-              `customer_orders__element-order-id-${id}`
-            }
-          >
-            { id }
-          </td>
-          <td
-            data-testid={
-              `customer_orders__element-delivery-status-${id}`
-            }
-          >
-            { status }
-          </td>
-          <td
-            className="text-center align-center"
-            width={ 150 }
-            data-testid={
-              `customer_orders__element-order-date-${id}`
-            }
-          >
-            { saleDate }
-          </td>
-          <td
-            className="text-center"
-            width={ 150 }
-            data-testid={
-              `customer_orders__element-card-price-${id}`
-            }
-          >
-            { totalPrice }
-          </td>
+const TableProducts = ({ sales }) => {
+  const navigate = useNavigate();
+
+  const navigateForSaleDetails = (id) => {
+    navigate(`/customer/orders/${id}`);
+  };
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr className="text-center">
+          <th>Pedido</th>
+          <th>Status</th>
+          <th>Data</th>
+          <th>Valor Total</th>
         </tr>
-      )) : <p>Você não possui pedidos.</p> }
-    </tbody>
-  </Table>
-);
+      </thead>
+      <tbody>
+        { sales.length ? sales.map(({ id, status, saleDate, totalPrice }, index) => (
+          <tr
+            onClick={ () => navigateForSaleDetails(id) }
+            key={ index }
+          >
+            <td
+              className="text-center"
+              width={ 1 }
+              data-testid={
+                `customer_orders__element-order-id-${id}`
+              }
+            >
+              { id }
+            </td>
+            <td
+              data-testid={
+                `customer_orders__element-delivery-status-${id}`
+              }
+            >
+              { status }
+            </td>
+            <td
+              className="text-center align-center"
+              width={ 150 }
+              data-testid={
+                `customer_orders__element-order-date-${id}`
+              }
+            >
+              { saleDate }
+            </td>
+            <td
+              className="text-center"
+              width={ 150 }
+              data-testid={
+                `customer_orders__element-card-price-${id}`
+              }
+            >
+              { totalPrice }
+            </td>
+          </tr>
+        )) : <p>Você não possui pedidos.</p> }
+      </tbody>
+    </Table>
+  );
+};
 
 TableProducts.propTypes = {
   sales: PropTypes.shape({
